5962f637d56db6a23de97e1180c47d11169d6f1e
esley <famillybeto@gmail.com>
2024-05-24 15:54:30 -0300

    Desafio-02-Cdd-4.0

--- classes.py
+++ classes.py
@@ -6,34 +6,34 @@ class Jogador():
         self.escolha = None
 
     def escolher(self):
-        escolhas = ['Pedra', 'Papel', 'Tesoura']
+        escolhas = ["Pedra", "Papel", "Tesoura"]
         self.escolha = random.choice(escolhas)
-        print(f'{self.nome} escolheu {self.escolha}')
+        print(f"{self.nome} escolheu {self.escolha}")
         return self.escolha
 
 class JogadorHumano(Jogador):
     def escolher(self):
         while True:
-            escolha = input(f'{self.nome}, escolha entre Pedra, Papel ou Tesoura: ').capitalize()
-            if escolha in ['Pedra', 'Papel', 'Tesoura']:
+            escolha = input(f"{self.nome}, escolha entre Pedra, Papel ou Tesoura: ").capitalize()
+            if escolha in ["Pedra", "Papel", "Tesoura"]:
                 self.escolha = escolha
-                print(f'{self.nome} escolheu {self.escolha}')
+                print(f"{self.nome} escolheu {self.escolha}")
                 return self.escolha
             else:
                 print("Escolha inválida, tente novamente.")
 
 class Jogo():
     def __init__(self):
-        self.JogadorHumano = JogadorHumano('Pessoa')
-        self.computador = Jogador('O computador')
+        self.JogadorHumano = JogadorHumano("Pessoa")
+        self.computador = Jogador("O computador")
 
     def determinarVencedor(self, escolha1, escolha2):
         if escolha1 == escolha2:
             return "Empate"
-        elif (escolha1 == 'Pedra' and escolha2 == 'Tesoura') or (escolha1 == 'Papel' and escolha2 == 'Pedra') or (escolha1 == 'Tesoura' and escolha2 == 'Papel'):
-            return f'{self.JogadorHumano.nome} venceu!'
+        elif (escolha1 == "Pedra" and escolha2 == "Tesoura") or (escolha1 == "Papel" and escolha2 == "Pedra") or (escolha1 == "Tesoura" and escolha2 == "Papel"):
+            return f"{self.JogadorHumano.nome} venceu!"
         else:
-            return f'{self.computador.nome} venceu!'
+            return f"{self.computador.nome} venceu!"
 
     def jogar(self):
         escolha1 = self.JogadorHumano.escolher()
